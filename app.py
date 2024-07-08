f91b742244efe35fbf37bae53fa1091360e7d38d
Rish <rishabhkartik2004@gmail.com>
2024-07-08 18:07:41 +0530

    basic dasboard added

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ def login_page():
 def sign_up():
     return render_template('signup.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__=="__main__":
     app.run(debug=True)
